95238d5ef160c4bdcc4ab264dad249fd11c7b845
thomasrmn <thomasromano2022@gmail.com>
2025-10-08 11:49:52 +0200

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,11 +1,10 @@
 import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
 import { get } from '@vercel/edge-config'
 
 export const config = { matcher: ['/qr'] }
 
-export default async function middleware(_req: NextRequest) {
-  const target = (await get<string>('qr_url')) || 'https://helpytravel.com'
+export default async function middleware() {
+  const target = (await get('qr_url')) || 'https://helpytravel.com'
   const res = NextResponse.redirect(target, 302)
   res.headers.set('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
   res.headers.set('Pragma', 'no-cache')
