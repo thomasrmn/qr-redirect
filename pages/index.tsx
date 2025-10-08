3043853fe6a4bba7de1b523c23957599888d8847
thomasrmn <thomasromano2022@gmail.com>
2025-10-08 11:41:11 +0200

    Create index.tsx
    paginetta di cortesia

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{padding:20,fontFamily:'sans-serif'}}>
+      <h1>QR Redirect</h1>
+      <p>Scansiona <code>/qr</code> per essere reindirizzato alla pagina impostata.</p>
+    </main>
+  )
+}
